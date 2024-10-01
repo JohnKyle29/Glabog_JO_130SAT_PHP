74d06ef1337e95fda4ffa18b00a9975a54736e90
JohnKyle29 <glabog1@gmail.com>
2024-10-01 17:45:25 +0800

    0921_Vid4_Two Key Eloquent Relationship Types

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,10 +1,17 @@
 <?php
+
 namespace App\Models;
-use Illuminate\Support\Arr;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class job extends Model {
     use HasFactory;
     protected $table = 'job_listings';
-    protected $fillable = ['title','salary'];
+
+    protected $fillable = ['title', 'salary'];
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
