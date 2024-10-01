be9323dff2940d50b4db47be866d99117ced3892
JohnKyle29 <glabog1@gmail.com>
2024-10-01 17:42:42 +0800

    0921_Vid3_Model Factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,7 +2,9 @@
 namespace App\Models;
 use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class job extends Model {
+    use HasFactory;
     protected $table = 'job_listings';
     protected $fillable = ['title','salary'];
 }
