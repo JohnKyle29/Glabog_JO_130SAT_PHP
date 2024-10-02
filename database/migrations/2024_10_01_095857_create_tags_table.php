e8d39e383241de2c6b1a428e184ad92222cc1603
JohnKyle29 <glabog1@gmail.com>
2024-10-03 03:24:26 +0800

    0921_Vid13_Eager Loading and the N+1 Problem_final

--- database/migrations/2024_10_01_095857_create_tags_table.php
+++ database/migrations/2024_10_01_095857_create_tags_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-          
+            $table->string('name');
             $table->timestamps();
         });
         Schema::create('job_tag', function (Blueprint $table) {
