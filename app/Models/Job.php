42cadee090c0bf3a48f4bbb663b4af82c5ee883d
JohnKyle29 <glabog1@gmail.com>
2024-10-03 04:03:26 +0800

    0928_Vid16_Forms and CSRF Explained(with Examples)

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,15 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class job extends Model {
     use HasFactory;
+
     protected $table = 'job_listings';
 
-    protected $fillable = ['title', 'salary'];
+    protected $guarded = [];
+
     public function employer()
     {
         return $this->belongsTo(Employer::class);
     }
-    public function tags()
-    {
-        return $this->belongsToMany(Tag::class,foreignPivotKey:"job_listing_id");
-    }
 }
+
