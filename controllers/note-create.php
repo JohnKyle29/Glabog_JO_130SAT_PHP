b896ce3282efd5e9b3b86113919a9630e85d6d73
JohnKyle29 <glabog1@gmail.com>
2024-10-05 03:31:43 +0800

    0928_Vid25_Intro to Forms and Request Methods

---
+++ controllers/note-create.php
@@ -0,0 +1,6 @@
+<?php
+$heading = 'Create Note';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Next episode: Create new note using form data from $_POST.
+}
+require 'views/note-create.view.php';
