8b24ed2efbd099fa60238d63264773b490f57c7a
JohnKyle29 <glabog1@gmail.com>
2024-10-05 05:17:36 +0800

    1005_Vid34_One Request, One Controller

--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -1,11 +1,12 @@
 <?php require base_path('views/partials/head.php') ?>
 <?php require base_path('views/partials/nav.php') ?>
 <?php require base_path('views/partials/banner.php') ?>
+
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <div class="md:grid md:grid-cols-3 md:gap-6">
             <div class="mt-5 md:col-span-2 md:mt-0">
-                <form method="POST">
+                <form method="POST" action="/notes">
                     <div class="shadow sm:overflow-hidden sm:rounded-md">
                         <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
                             <div>
@@ -22,6 +23,7 @@
                                         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                                         placeholder="Here's an idea for a note..."
                                     ><?= $_POST['body'] ?? '' ?></textarea>
+
                                     <?php if (isset($errors['body'])) : ?>
                                         <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
                                     <?php endif; ?>
