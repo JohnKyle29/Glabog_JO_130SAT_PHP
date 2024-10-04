a31cd2c062c8a7c27ceb52b7b1e2ae77de3c6e3d
JohnKyle29 <glabog1@gmail.com>
2024-10-05 03:11:39 +0800

    0921_Vid18_Extract a PHP Database Class

--- index.php
+++ index.php
@@ -4,12 +4,7 @@ require 'functions.php';
 //require 'router.php';
 require 'Database.php';
 
-$config = require('config.php');
-$db = new Database($config['database']);
-
-$id = $_GET['id'];
-$query = "select * from posts where id = :id";
-
-$posts = $db->query($query, [':id' => $id])->fetch();
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
 dd($posts);
