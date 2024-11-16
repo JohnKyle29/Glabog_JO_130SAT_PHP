6988005fc396fedb101346867e4447c255960429
JohnKyle29 <glabog1@gmail.com>
2024-11-16 21:59:23 +0800

    1116_Vid43_Extract and Authenticator Class

--- Core/Authenticator.php
+++ Core/Authenticator.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Core;
+
 class Authenticator
 {
     public function attempt($email, $password)
@@ -8,28 +10,31 @@ class Authenticator
             ->query('select * from users where email = :email', [
             'email' => $email
         ])->find();
+
         if ($user) {
             if (password_verify($password, $user['password'])) {
                 $this->login([
                     'email' => $email
                 ]);
+
                 return true;
             }
         }
+
         return false;
     }
+
     public function login($user)
     {
         $_SESSION['user'] = [
             'email' => $user['email']
         ];
+
         session_regenerate_id(true);
     }
+
     public function logout()
     {
-        $_SESSION = [];
-        session_destroy();
-        $params = session_get_cookie_params();
-        setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+        Session::destroy();
     }
 }
